b81a66e9d22f7d5d066e39dd1e24386eb6bcf334
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-04 13:41:53 +0100

    refactored obect initialisation in tests.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,24 +1,22 @@
 require 'checkout'
 
 RSpec.describe Shop do
+  subject(:shop) { described_class.new }
+  
   it "#checkout takes ('A') and returns 50" do
-    shop = Shop.new
     expect(shop.checkout('A')).to eq 50
   end
 
   it "#checkout takes ('B') and returns 30" do
-    shop = Shop.new
     expect(shop.checkout('B')).to eq 30
   end
 
   it "#checkout takes ('C') and returns 20" do
-    shop = Shop.new
     expect(shop.checkout('C')).to eq 20
   end
 
   it "#checkout takes ('D') and returns 15" do
-  shop = Shop.new
   expect(shop.checkout('D')).to eq 15
   end
-  
+
 end
