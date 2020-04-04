9e8cccaa486601eb10d7f410a98d40aa2553b68f
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-04-04 14:04:40 +0100

    refactored tests into contexts.

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -3,24 +3,28 @@ require 'checkout'
 RSpec.describe Shop do
   subject(:shop) { described_class.new }
 
-  it "#checkout takes ('A') and returns 50" do
-    expect(shop.checkout('A')).to eq 50
+  context "individual items" do
+    it "#checkout takes ('A') and returns 50" do
+      expect(shop.checkout('A')).to eq 50
+    end
+
+    it "#checkout takes ('B') and returns 30" do
+      expect(shop.checkout('B')).to eq 30
+    end
+
+    it "#checkout takes ('C') and returns 20" do
+      expect(shop.checkout('C')).to eq 20
+    end
+
+    it "#checkout takes ('D') and returns 15" do
+    expect(shop.checkout('D')).to eq 15
+    end
   end
 
-  it "#checkout takes ('B') and returns 30" do
-    expect(shop.checkout('B')).to eq 30
-  end
-
-  it "#checkout takes ('C') and returns 20" do
-    expect(shop.checkout('C')).to eq 20
-  end
-
-  it "#checkout takes ('D') and returns 15" do
-  expect(shop.checkout('D')).to eq 15
-  end
-
-  it "#checkout takes ('AA') and returns 100" do
-  expect(shop.checkout('AA')).to eq 100
+  context "discounts" do
+    it "#checkout takes ('AA') and returns 100" do
+    expect(shop.checkout('AA')).to eq 100
+    end
   end
 
 end
